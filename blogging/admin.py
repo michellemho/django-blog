668f884386cdf629320ec34561bf4d4a232f909a
michelle ho <michellemho@users.noreply.github.com>
2022-07-11 22:13:50 -0700

    change to custom inline model

--- blogging/admin.py
+++ blogging/admin.py
@@ -4,5 +4,25 @@ from blogging.models import Category
 
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Category)
+# admin.site.register(Post)
+# admin.site.register(Category)
+
+class CategorizationInline(admin.TabularInline):
+    model = Category.posts.through
+    extra = 0
+    max_num = 4
+    min_num = 0
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategorizationInline,
+    ]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # inlines = [
+    #     CategorizationInline,
+    # ]
+    exclude = ['posts']
