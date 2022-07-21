d14bdbc3334dee7c390e3267007268d5eb978159
michelle ho <michellemho@users.noreply.github.com>
2022-07-21 14:53:23 -0700

    updating to django class based views

--- polling/views.py
+++ polling/views.py
@@ -1,23 +1,43 @@
 from django.shortcuts import render
 from django.http import Http404
 from polling.models import Poll
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 
-def list_view(request):
-	context = {'polls': Poll.objects.all()}
-	return render(request, 'polling/list.html', context)
+# def list_view(request):
+# 	context = {'polls': Poll.objects.all()}
+# 	return render(request, 'polling/list.html', context)
 
-def detail_view(request, poll_id):
-	try:
-		poll = Poll.objects.get(pk=poll_id)
-	except Poll.DoesNotExist:
-		raise Http404
-	if request.method == "POST":
+class PollListView(ListView):
+	model = Poll
+	template_name = 'polling/list.html'
+
+class PollDetailView(DetailView):
+	model = Poll
+	template_name = 'polling/detail.html'
+	
+	# need to be able to do one more thing, post votes... add method here.
+	def post(self, request, *args, **kwargs):
+		poll = self.get_object() # retrieve ID of indicated poll
 		if request.POST.get("vote") == "Yes":
 			poll.score += 1
 		else:
 			poll.score -= 1
-		poll.save()
-	context = {'poll': poll}
-	return render(request, 'polling/detail.html', context)
+		context = {'object': poll} #context variable named object... generic
+		return render(request, 'polling/detail.html', context)
+
+# def detail_view(request, poll_id):
+# 	try:
+# 		poll = Poll.objects.get(pk=poll_id)
+# 	except Poll.DoesNotExist:
+# 		raise Http404
+# 	if request.method == "POST":
+# 		if request.POST.get("vote") == "Yes":
+# 			poll.score += 1
+# 		else:
+# 			poll.score -= 1
+# 		poll.save()
+# 	context = {'poll': poll}
+# 	return render(request, 'polling/detail.html', context)
