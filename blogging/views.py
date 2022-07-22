6e5b4f120c312c820c9b8bf55972a5e6a193d105
michelle ho <michellemho@users.noreply.github.com>
2022-07-21 17:57:33 -0700

    try to update blog list view

--- blogging/views.py
+++ blogging/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.template import loader
 
 from blogging.models import Post
@@ -16,13 +17,21 @@ def stub_view(request, *args, **kwargs):
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
 
-def list_view(request):
-    published = Post.objects.exclude(publish_datetime__exact=None)
-    posts = published.order_by('-publish_datetime')
-    template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    body = template.render(context)
-    return HttpResponse(body, content_type="text/html")
+def BlogListView(ListView):
+    queryset = Post.objects.exclude(publish_datetime__exact=None).order_by('-publish_datetime')
+    # in addtion to getting lower-cased model name + _list "like object_list, post_list"... you can define context_object_name
+    # read more about context_object_name:
+    #  https://docs.djangoproject.com/en/2.1/topics/class-based-views/generic-display/#making-friendly-template-contexts
+    context_object_name = 'reverse_published_posts'
+    template_name = 'blogging/list.html'
+
+# def list_view(request):
+#     published = Post.objects.exclude(publish_datetime__exact=None)
+#     posts = published.order_by('-publish_datetime')
+#     template = loader.get_template('blogging/list.html')
+#     context = {'posts': posts}
+#     body = template.render(context)
+#     return HttpResponse(body, content_type="text/html")
 
     # rewrite our view with render shortcut provided by Django
     # return render(request, 'blogging/list.html', context)
