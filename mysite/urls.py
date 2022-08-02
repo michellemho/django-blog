e430c18d027bf56468c2f243c284d881cdd6ff12
michelle ho <michellemho@users.noreply.github.com>
2022-08-02 12:56:31 -0700

    adding github auth... kinda...

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib.auth.views import LoginView, LogoutView
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include # new
+# from .views import Home # new
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -24,4 +26,7 @@ urlpatterns = [
     path("", include("blogging.urls")),
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
+    path('accounts/', include('allauth.urls'), name='account_login'), # new
+    # path('', Home.as_view(), name='home'), # new
+
 ]
